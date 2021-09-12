ea479e53cd183a76da18bdcb377f92b57e699f38
iigmir <roc120j@gmail.com>
2021-09-12 18:54:51 +0800

    Clicked

--- src/views/App.js
+++ src/views/App.js
@@ -8,15 +8,23 @@ class App extends React.Component {
     constructor(props)
     {
         super(props);
-        this.state = { entries };
+        this.state = { entries, hide_modal: true, };
+    }
+    toggle_modal_flag()
+    {
+        const hide_modal = !this.state.hide_modal;
+        this.setState({ hide_modal });
     }
     render()
     {
         return (
             <div className="App">
                 <Header />
-                <Card entries={ entries } />
-                <Modal />
+                <div className="operation container">
+                    <button className="button" onClick={ () => this.toggle_modal_flag() }>Button</button>
+                </div>
+                <Card entries={ this.state.entries } />
+                <Modal hidden={ this.state.hide_modal } toggle={ () => this.toggle_modal_flag() } />
             </div>
         );
     }
--- src/views/Modal/index.js
+++ src/views/Modal/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./modal.css";
 
 class Modal extends React.Component {
@@ -7,21 +8,30 @@ class Modal extends React.Component {
         super(props);
         this.state = { hide: true, };
     }
+    shall_we_close(e)
+    {
+        const clicked_on_overlay = e.target === e.currentTarget;
+        const clicked_on_close_button = Boolean( e.target.dataset.closeModal );
+        if( clicked_on_overlay || clicked_on_close_button )
+        {
+            this.props.toggle();
+        }
+    }
     render()
     {
         // modal-box
         // kube-fadeIn
         // kube-fadeOut
         // modal is-open
-        if( this.state.hide )
+        if( this.props.hidden )
         {
             return (<div />);
         }
         return (
             <div id="create-modal" className="modal-container">
-                <div className="overlay">
+                <div className="overlay" onClick={ (dom) => this.shall_we_close(dom) }>
                     <div className="modal is-open">
-                        <span className="close"></span>
+                        <span className="close" data-close-modal="true"></span>
                         <div className="modal-header">My Modal</div>
                         <div className="modal-body">
                             <p>Lorem ipsum dolor sit amet...</p>
@@ -33,5 +43,10 @@ class Modal extends React.Component {
         );
     }
 }
+
+Modal.propTypes = {
+    hidden: PropTypes.bool,
+    toggle: PropTypes.func
+};
 
 export default Modal;
