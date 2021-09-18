cf0ff35f21c5e192673e97886157fb28ee2d8dfe
iigmir <roc120j@gmail.com>
2021-09-18 09:46:19 +0800

    HTML refactor

--- src/views/Modal/index.js
+++ src/views/Modal/index.js
@@ -88,70 +88,75 @@ class Modal extends React.Component {
                         { JSON.stringify(item) }
                     </li>) ) }</ul>
                 </div>
-            </div>) : (
-            <div className="is-row"></div>
+            </div>) :
+            ( <div className="is-row"></div> )
+        ;
+        const form = (
+            <form onSubmit={ e => this.add_entry(e) }>
+                <div className="is-row">
+                    <div className="form-item is-col is-80">
+                        <label htmlFor="word">Word</label>
+                        <input id="word" type="text" name="word" value={ this.state.word } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-20">
+                        <label htmlFor="language">Language</label>
+                        <input id="language" type="text" name="language" value={ this.state.language } onChange={ e => this.change_state(e) } />
+                    </div>
+                </div>
+                <div className="terms is-row">
+                    <input id="terms" type="hidden" name="terms" value={ this.state.terms } />
+                    <div className="form-item is-col is-50">
+                        <label htmlFor="term-description">Terms: Description</label>
+                        <input id="term-description" type="text" name="term-description" value={ this.state["term-description"] } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-20">
+                        <label htmlFor="term-language">Terms: Language</label>
+                        <input id="term-language" type="text" name="term-language" value={ this.state["term-language"] } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-20">
+                        <label htmlFor="pos">Part of speech</label>
+                        <input id="pos" type="text" name="pos" value={ this.state.pos } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-10">
+                        <label>&nbsp;</label>
+                        <button className="button is-secondary" onClick={ e => this.push_array("terms", e) } type="button">Add</button>
+                    </div>
+                </div>
+                { list_comp(this.state.terms, "terms") }
+                <div className="references is-row">
+                    <input id="references" type="hidden" name="references" value={ this.state.references } />
+                    <div className="form-item is-col is-50">
+                        <label htmlFor="reference-href">Reference: URL</label>
+                        <input id="reference-href" type="text" name="reference-href" value={ this.state["reference-href"] } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-40">
+                        <label htmlFor="reference-text">References: Text</label>
+                        <input id="reference-text" type="text" name="reference-text" value={ this.state["reference-text"] } onChange={ e => this.change_state(e) } />
+                    </div>
+                    <div className="form-item is-col is-10">
+                        <label>&nbsp;</label>
+                        <button className="button is-secondary" onClick={ e => this.push_array("references", e) } type="button">Add</button>
+                    </div>
+                </div>
+                { list_comp(this.state.references, "references") }
+                <div className="form-item is-buttons">
+                    <button type="submit" className="button">Add entry</button>
+                </div>
+            </form>
+        );
+        const content = (
+            <div className="modal is-open">
+                <span className="close" data-close-modal="true"></span>
+                <div className="modal-header">My Modal</div>
+                <div className="modal-body">{ form }</div>
+                <div className="modal-footer"></div>
+            </div>
         );
         return (
             <div id="create-modal" className="modal-container">
                 <div className="overlay" onClick={ (dom) => this.shall_we_close(dom) }>
-                    <div className="modal is-open">
-                        <span className="close" data-close-modal="true"></span>
-                        <div className="modal-header">My Modal</div>
-                        <div className="modal-body">
-                            <form onSubmit={ e => this.add_entry(e) }>
-                                <div className="is-row">
-                                    <div className="form-item is-col is-80">
-                                        <label htmlFor="word">Word</label>
-                                        <input id="word" type="text" name="word" value={ this.state.word } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-20">
-                                        <label htmlFor="language">Language</label>
-                                        <input id="language" type="text" name="language" value={ this.state.language } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                </div>
-                                <div className="terms is-row">
-                                    <input id="terms" type="hidden" name="terms" value={ this.state.terms } />
-                                    <div className="form-item is-col is-50">
-                                        <label htmlFor="term-description">Terms: Description</label>
-                                        <input id="term-description" type="text" name="term-description" value={ this.state["term-description"] } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-20">
-                                        <label htmlFor="term-language">Terms: Language</label>
-                                        <input id="term-language" type="text" name="term-language" value={ this.state["term-language"] } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-20">
-                                        <label htmlFor="pos">Part of speech</label>
-                                        <input id="pos" type="text" name="pos" value={ this.state.pos } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-10">
-                                        <label>&nbsp;</label>
-                                        <button className="button is-secondary" onClick={ e => this.push_array("terms", e) } type="button">Add</button>
-                                    </div>
-                                </div>
-                                { list_comp(this.state.terms, "terms") }
-                                <div className="references is-row">
-                                    <input id="references" type="hidden" name="references" value={ this.state.references } />
-                                    <div className="form-item is-col is-50">
-                                        <label htmlFor="reference-href">Reference: URL</label>
-                                        <input id="reference-href" type="text" name="reference-href" value={ this.state["reference-href"] } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-40">
-                                        <label htmlFor="reference-text">References: Text</label>
-                                        <input id="reference-text" type="text" name="reference-text" value={ this.state["reference-text"] } onChange={ e => this.change_state(e) } />
-                                    </div>
-                                    <div className="form-item is-col is-10">
-                                        <label>&nbsp;</label>
-                                        <button className="button is-secondary" onClick={ e => this.push_array("references", e) } type="button">Add</button>
-                                    </div>
-                                </div>
-                                { list_comp(this.state.references, "references") }
-                                <div className="form-item is-buttons">
-                                    <button type="submit" className="button">Add entry</button>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer"></div>
-                    </div>
+                    <div className="modal is-open"></div>
+                    { content }
                 </div>
             </div>
         );
