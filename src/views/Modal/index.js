d0f84385f055a1659db75e444cf3518f3b21f480
iigmir <roc120j@gmail.com>
2021-09-18 22:44:42 +0800

    fix error

--- src/views/Modal/index.js
+++ src/views/Modal/index.js
@@ -11,7 +11,7 @@ class Modal extends React.Component {
             hide: true,
             word: "",
             language: "",
-            pos: "",
+            pos: PosSelections[0],
             terms: [],
             references: [],
             "term-description": "",
