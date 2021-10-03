8ff2c92359aaa1c020fbfacd832d43f3fe79ac27
iigmir <roc120j@gmail.com>
2021-10-03 08:57:19 +0800

    LanguageComponent

--- src/views/Modal/controlled-components.js
+++ src/views/Modal/controlled-components.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Languages from "../../assets/languages.json";
 
 export function WordComponent(props)
 {
@@ -10,5 +11,20 @@ export function WordComponent(props)
 }
 WordComponent.propTypes = {
     word: PropTypes.string,
+    changeState: PropTypes.func,
+};
+
+export function LanguageComponent(props)
+{
+    return (<div className="form-item is-col is-20">
+        <label htmlFor="language">Language</label>
+        <select id={ props.stateName } name={ props.stateName } value={ props.stateValue } onChange={ e => props.changeState(e) }>
+            { Languages.map( ({ text, value }) => (<option key={ value } value={ value }>{ text }</option>) ) }
+        </select>
+    </div>);
+}
+LanguageComponent.propTypes = {
+    stateValue: PropTypes.string,
+    stateName: PropTypes.string,
     changeState: PropTypes.func,
 };
